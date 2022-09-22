c31010b2b6af1b13b4d44d64a9a49f9039481312
Ptashenko Maxim <theykropchik@gmail.com>
2022-09-22 19:55:00 +0400

    mint

---
+++ firstSteps/modules/functions.js
@@ -0,0 +1,35 @@
+function addTodo () {
+    const todo = {
+        name:document.getElementById("famale").value,
+        done:false,
+        id: `${Math.random()}`
+    }
+    todos.push(todo);
+    render();
+}
+
+function addEquipment() {
+    const equipment = {
+        strength:document.getElementById("strength").value,
+        int:document.getElementById("int").value,
+        agility:document.getElementById("agility").value,
+    }
+    if (document.getElementById("sword") === 'true'){
+        alert("Hello");
+    }
+    equipments.push(equipment);
+    render();
+}
+
+function deleteTodo (id) {
+    todos.forEach(todo => {
+        if (todo.id === id){
+            todo.done = true;
+        }
+    })
+}
+
+function render() {
+    console.log(todos);
+    console.log(equipments);
+}
